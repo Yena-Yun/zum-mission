e7b6fa9af56582ebe8a778f82df8f1cee8dee6e7
Yena-Yun <cheryleduart@gmail.com>
2021-10-05 16:20:12 +0900

    life페이지 json값 렌더링

--- zum-router/index.js
+++ zum-router/index.js
@@ -1,47 +1,10 @@
 // css import
 require('./css/style.css');
-const bestData = require('./json/best.json');
-console.log(bestData);
+
 // 라우터 import
 const { initialRoutes, historyRouterPush } = require('./router');
 
-const bestId = bestData.map((el, id) => id + 1);
-console.log(bestId);
-const bestmediaName = bestData.map((el) => el.mediaName);
-const bestTitle = bestData.map((el) => el.title);
-const bestUrl = bestData.map((el) => el.url);
-
-// // state를 기반으로 렌더링
-let state = { id: bestId, mediaName: bestmediaName, title: bestTitle, url: bestUrl };
-console.log(state);
-
 const $app = document.querySelector('#app');
-
-// 렌더 함수
-const bestRender = ({ id, mediaName, title, url }) => {
-  $app.innerHTML = `
-  <a href="${url}" class="rank-div">
-    <h1>${id}</h1>
-    <div>
-	    <p>${title}</p>
-	    <p>${mediaName}</p>
-    </div>
-  </a>
-  `;
-};
-
-bestRender(state);
-// 각 라우터의 내용으로 교체될 DOM
-console.log($app);
-
-// // state 수정하는 함수
-// // 실행 시 state에 새 state를 추가하여 다시 렌더링
-
-// // 렌더 함수에 state를 넣어 렌더링
-
-// // state 수정
-// setState({ title: 'Hello ' });
-// setState({ description: 'World ' });
 
 // 기본 경로
 initialRoutes('history', $app);
--- zum-router/router.js
+++ zum-router/router.js
@@ -5,6 +5,9 @@ const tourTemplate = require('./pages/tour.hbs');
 const cultureTemplate = require('./pages/culture.hbs');
 const favoriteTemplate = require('./pages/favorite.hbs');
 
+const bestData = require('./json/best.json');
+const lifeData = require('./json/life.json');
+
 const Home = homeTemplate();
 const Life = lifeTemplate();
 const Food = foodTemplate();
@@ -22,8 +25,61 @@ const routes = {
   '/favorite': Favorite,
 };
 
+const bestId = bestData.map((el, id) => id + 1);
+const bestMediaName = bestData.map((el) => el.mediaName);
+const bestTitle = bestData.map((el) => el.title);
+const bestUrl = bestData.map((el) => el.url);
+
+// // state를 기반으로 렌더링
+let bestState = { id: bestId, mediaName: bestMediaName, title: bestTitle, url: bestUrl };
+
+// 렌더 함수
+const bestRender = ({ id, mediaName, title, url }, el) => {
+  el.innerHTML = `
+  <div>
+    <h1>${id}</h1>
+    <div>
+	    <p>${title}</p>
+	    <p>${mediaName}</p>
+    </div>
+  </div>
+  `;
+};
+
+const lifeId = lifeData.map((el, id) => id + 1);
+const lifeTitle = lifeData.map((el) => el.title);
+const lifeImageUrl = lifeData.map((el) => el.mediaName);
+const lifeMediaName = lifeData.map((el) => el.mediaName);
+const lifeUrl = lifeData.map((el) => el.url);
+const lifeSummary = lifeData.map((el) => el.url);
+
+// // state를 기반으로 렌더링
+let lifeState = {
+  id: lifeId,
+  title: lifeTitle,
+  imageUrl: lifeImageUrl,
+  mediaName: lifeMediaName,
+  url: lifeUrl,
+  summaryContent: lifeSummary,
+};
+
+// 렌더 함수
+const lifeRender = ({ id, title, imageUrl, mediaName, url, summaryContent }, el) => {
+  el.innerHTML = `
+  <div>
+    <img src=${imageUrl} alt="thumbnail-image" />
+	    <p>${title}</p>
+	    <p>${summaryContent}</p>
+	    <p>${mediaName}</p>
+  </div>
+  `;
+};
+
 function initialRoutes(mode, el) {
+  console.log(mode);
+  console.log(el);
   renderHTML(el, routes['/']);
+  renderHTML(el, routes['/life']);
 
   if (mode === 'history') {
     window.onpopstate = () => renderHTML(el, routes[window.location.pathname]);
@@ -32,11 +88,19 @@ function initialRoutes(mode, el) {
 
 function historyRouterPush(pathName, el) {
   window.history.pushState({}, pathName, window.location.origin + pathName);
+
+  console.log(pathName);
+  console.log(routes[pathName]);
   renderHTML(el, routes[pathName]);
 }
 
 function renderHTML(el, route) {
   el.innerHTML = route;
+
+  bestRender(bestState, el);
+  if (route === routes['/life']) {
+    lifeRender(lifeState, el);
+  }
 }
 
 module.exports = {
